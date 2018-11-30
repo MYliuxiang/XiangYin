445e736ecacd37ffda92d517031e96280ad099a5
liuxiang <liuxiang@imac-2.local>
2018-11-30 14:17:49 +0800

    Initial Commit

---
+++ XiangYin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiangYin
+//
+//  Created by liuxiang on 2018/11/30.
+//  Copyright © 2018年 liuxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
